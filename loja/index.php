0cebd6fb89d35c8a49dc9d07afcf65f92631aa18
Arie_Azzi <ariel_azzi@hotmail.com>
2016-11-02 00:52:49 -0200

    Criando formulario de login, e fazendo o acesso

--- loja/index.php
+++ loja/index.php
@@ -1,3 +1,30 @@
-<?php include("cabecalho.php"); ?>
+<?php
+include("cabecalho.php");
+
+if (isset($_GET['login']) && $_GET['login'] == true):
+?>
+<p class="alert-success">Logado com sucesso!</p>
+<?php endif ?>
+
+<?php if (isset($_GET['login']) && $_GET['login'] == false): ?>
+<p class="alert-danger">Usuário ou senha invalida!</p>
+<?php endif ?>
+
     <h1>Bem vindo!</h1>
+    <h2>Login</h2>
+    <form action="login.php" method="post">
+    	<table class="table">
+    		<tr>
+    			<td>Email</td>
+    			<td><input class="form-control" type="email" name="email"></td>
+    		</tr>
+    		<tr>
+    			<td>Senha</td>
+    			<td><input class="form-control" type="password" name="senha"></td>
+    		</tr>
+    		<tr>
+    			<td><button class="btn btn-primary" type="submit">Login</button></td>
+    		</tr>
+    	</table>
+    </form>
 <?php include("rodape.php"); ?>
