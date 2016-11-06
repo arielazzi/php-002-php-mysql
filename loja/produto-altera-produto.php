9ccb86ba7964663e857c36a04f42ec20b983f981
Arie_Azzi <ariel_azzi@hotmail.com>
2016-11-06 15:50:15 -0200

    criando apenas um formulario para adicionar e alterar

--- loja/produto-altera-produto.php
+++ loja/produto-altera-produto.php
@@ -16,38 +16,7 @@ $usado = $produto['usado'] ? "checked='checked'" : "";
     <form action="altera-produto.php" method="post">
     	<table class="table">
     	 	<input type="hidden" name="id" value="<?=$produto['id']?>">
-			<tr>
-				<td>Nome</td>
-				<td><input class="form-control" type="text" name="nome" value="<?=$produto['nome']?>" /></td>
-			</tr>
-			<tr>
-				<td>Preço</td>
-				<td><input class="form-control" type="number" name="preco" value="<?=$produto['preco']?>" /></td>
-			</tr>
-			<tr>
-				<td>Descrição</td>
-				<td><textarea class="form-control" name="descricao"><?=$produto['descricao']?></textarea></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td>
-					<input type="checkbox" name="usado" value="true" <?=$usado?> >Usado
-				</td>
-			</tr>
-			<tr>
-				<td>Categorias</td>
-				<td>
-					<select name="categoria_id" class="form-control">
-					<?php
-					foreach($categorias as $categoria):
-						$essaCategoria = $categoria['id'] == $produto['categoria_id'];
-						$selecao = $essaCategoria ? "selected='selected'" : "";
-					?>
-						<option  value="<?=$categoria['id']?>" <?=$selecao?> ><?=$categoria['nome']?></option>
-					<?php endforeach ?>
-					</select>
-				</td>
-			</tr>
+			<?php include("produto-formulario-base.php"); ?>
 			<tr>
 				<td>
         			<input class="btn btn-primary" type="submit" value="Alterar" />
--- loja/produto-formulario.php
+++ loja/produto-formulario.php
@@ -5,40 +5,14 @@ include("banco-categoria.php");
 include("logica-usuario.php");
 
 verificaUsuario();
-
+$produto = array('nome' => "", 'preco' => "", 'descricao' => "", 'categoria_id' => "1");
+$usado = false;
 $categorias = listaCategorias($conexao);
 ?>
 	<h1>Adiciona Produto</h1>
     <form action="adiciona-produto.php" method="post">
     	<table class="table">
-			<tr>
-				<td>Nome</td>
-				<td><input class="form-control" type="text" name="nome" /></td>
-			</tr>
-			<tr>
-				<td>Preço</td>
-				<td><input class="form-control" type="number" name="preco" /></td>
-			</tr>
-			<tr>
-				<td>Descrição</td>
-				<td><textarea class="form-control" name="descricao"></textarea></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td>
-					<input type="checkbox" name="usado" value="true">Usado
-				</td>
-			</tr>
-			<tr>
-				<td>Categorias</td>
-				<td>
-					<select name="categoria_id" class="form-control">
-					<?php foreach($categorias as $categoria): ?>
-						<option  value="<?=$categoria['id']?>"><?=$categoria['nome']?></option>
-					<?php endforeach ?>
-					</select>
-				</td>
-			</tr>
+			<?php include("produto-formulario-base.php"); ?>
 			<tr>
 				<td>
         			<input class="btn btn-primary" type="submit" value="Cadastrar" />
